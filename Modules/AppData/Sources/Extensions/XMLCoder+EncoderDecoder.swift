77521f35640e7194ad9c67ccafd6bbf58c418015
Jonas Frey <dev@jonasfrey.de>
2024-05-12 20:05:59 +0200

    Conform XMLCoder to AppNetworking Encoder/Decoder

---
+++ Modules/AppData/Sources/Extensions/XMLCoder+EncoderDecoder.swift
@@ -0,0 +1,9 @@
+// Copyright © 2024 Jonas Frey. All rights reserved.
+
+import AppNetworking
+import Foundation
+import XMLCoder
+
+extension XMLEncoder: Encoder {}
+
+extension XMLDecoder: Decoder {}
