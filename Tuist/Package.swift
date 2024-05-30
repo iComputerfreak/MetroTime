2f80e23d759af93776af2849a81b82b80ece7127
Jonas Frey <dev@jonasfrey.de>
2024-05-30 17:33:00 +0200

    Update JFUtils and remove SwiftLint rule

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Add your own dependencies here:
         // .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0"),
         // You can read more about dependencies here: https://docs.tuist.io/documentation/tuist/dependencies
-        .package(url: "https://github.com/iComputerfreak/JFUtils", from: "1.0.0"),
+        .package(url: "https://github.com/iComputerfreak/JFUtils", branch: "main"),
         .package(url: "https://github.com/CoreOffice/XMLCoder", from: "0.17.1"),
         .package(url: "https://github.com/hmlongco/Factory", from: "2.3.0"),
     ]
